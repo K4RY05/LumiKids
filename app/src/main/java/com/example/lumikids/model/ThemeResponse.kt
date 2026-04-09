c56d26b7e74b297c570912187a5b7397ece633dc
CarlosNexuz <carvillasa@gmail.com>
2026-04-09 16:57:26 -0600

    Eliminar archivos obsoleto

--- app/src/main/java/com/example/lumikids/model/ThemeResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lumikids.model
-
-data class ThemeResponse(
-    val ID_theme: Int,
-    val nametheme: String,
-    val linktheme: String
-)
--- app/src/main/java/com/example/lumikids/network/GameApi.kt
+++ app/src/main/java/com/example/lumikids/network/GameApi.kt
@@ -1,27 +1,11 @@
 package com.example.lumikids.network
 
-import com.example.lumikids.model.ThemeResponse
-import com.example.lumikids.model.SoundResponse
 import com.example.lumikids.model.GameObject
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface GamesApi {
-
-    @GET("api/games/themes/{id_category}")
-    fun getThemesByCategory(
-        @Path("id_category") categoryId: Int
-    ): Call<List<ThemeResponse>>
-
-    @GET("api/games/sounds/{id_category}")
-    fun getSoundsByCategory(
-        @Path("id_category") categoryId: Int,
-        @Query("filter") filter: String? = null
-    ): Call<List<SoundResponse>>
-
-    // ✨ ESTA ES LA NUEVA FUNCIÓN QUE TE ESTÁ PIDIENDO EL CONTROLADOR ✨
     @GET("api/games/game-objects/{id_category}")
     fun getGameObjects(
         @Path("id_category") categoryId: Int
