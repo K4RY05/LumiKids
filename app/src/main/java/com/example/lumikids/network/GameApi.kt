1b546d0d8176974012ece0164bc5767e8a5d91f3
CarlosNexuz <carvillasa@gmail.com>
2026-05-03 14:43:58 -0600

    Filtro de los elementos de cada categoria

--- app/src/main/java/com/example/lumikids/network/GameApi.kt
+++ app/src/main/java/com/example/lumikids/network/GameApi.kt
@@ -4,11 +4,12 @@ import com.example.lumikids.model.GameObject
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-
+import retrofit2.http.Query
 interface GameApi {
 
-    @GET("api/games/game-objects/{id_category}")
+    @GET("api/games/game-objects/{categoryId}")
     fun getGameObjects(
-        @Path("id_category") categoryId: Int
+        @Path("categoryId") categoryId: Int,
+        @Query("juego") forGame: Boolean = true
     ): Call<List<GameObject>>
 }
